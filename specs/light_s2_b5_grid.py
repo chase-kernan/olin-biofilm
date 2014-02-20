0f82f1775bf9cbc3a5ce678936b1a181d32ecb75
Chase Kernan <chase.kernan@gmail.com>
2014-02-19 20:57:23 -0500

    Grid searches

--- specs/light_s2_b5_grid.py
+++ specs/light_s2_b5_grid.py
@@ -1,6 +1,6 @@
 
 from biofilm import util
-util.set_h5(util.results_h5_path('aerobic_light0_s2_b5_grid'))
+util.set_h5(util.results_h5_path('light_s2_b5_grid'))
 
 from biofilm.model.spec import Spec, SpecBuilder
 from biofilm.model import runner, result
@@ -9,11 +9,11 @@ import numpy as np
 builder = SpecBuilder()
 builder.add('stop_on_mass', 5000)
 builder.add('boundary_layer', 5)
-builder.add('light_penetration', 0)
+builder.add('light_penetration', 1, 2, 4, 8, 16)
 builder.add('tension_power', 2)
 builder.add('initial_cell_spacing', 2)
 builder.add('media_ratio', *np.linspace(0.1, 5.0, 10))
-builder.add('media_monod', *np.linspace(0.1, 1, 10))
+builder.add('media_monod', *np.linspace(0.1, 1, 5))
 builder.add('light_monod', *np.linspace(0.1, 1, 10))
 total = builder.num_specs
 builder.build()
---
+++ specs/light_s32_b5_grid.py
@@ -0,0 +1,25 @@
+
+from biofilm import util
+util.set_h5(util.results_h5_path('light_s2_b5_grid'))
+
+from biofilm.model.spec import Spec, SpecBuilder
+from biofilm.model import runner, result
+import numpy as np
+
+builder = SpecBuilder()
+builder.add('stop_on_mass', 4000)
+builder.add('boundary_layer', 5)
+builder.add('light_penetration', 1, 2, 4, 8, 16)
+builder.add('tension_power', 2)
+builder.add('initial_cell_spacing', 32)
+builder.add('media_ratio', *np.linspace(0.1, 5.0, 10))
+builder.add('media_monod', *np.linspace(0.1, 1, 5))
+builder.add('light_monod', *np.linspace(0.1, 1, 10))
+total = builder.num_specs
+builder.build()
+
+for i, spec in enumerate(Spec.all()):
+    print i, 'of', total
+    result.from_model(runner.run(spec)).save()
+
+print 'done!'
