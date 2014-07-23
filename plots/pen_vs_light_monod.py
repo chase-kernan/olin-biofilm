8ec6c91dabb7d6d915f1fb59df99875c6bc00bc9
Chase Kernan <chase.kernan@gmail.com>
2014-07-22 19:08:15 -0700

    light

--- plots/pen_vs_light_monod.py
+++ plots/pen_vs_light_monod.py
@@ -13,71 +13,102 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 import pickle
+import sys
 
+command = sys.argv[1]
 
-# LIGHT
-# -----
+if command == 'dump':
+    num_specs = len(sp.Spec)
+    print num_specs
+    dtype = [('pen', float),
+             ('monod', float),
+             ('mass', float),
+             ('perimeter', float),
+             ('hsa', float),
+             ('convex_density', float),
+             ('cell_height', float)]
+    data = np.empty(num_specs, dtype)
 
-def compute():
-    an.mass.compute_specs(recompute=True)
-    #an.convex_density.compute_specs(recompute=True)
-    an.mean_cell_height.compute_specs(recompute=True)
-    # an.heights.compute_specs(recompute=True)
-    an.perimeter.compute_specs(recompute=True)
-    # an.roughness.compute_specs(recompute=True)
-    # an.mean_overhang.compute_specs(recompute=True)
-    an.horizontal_surface_area.compute_specs()
-# compute()
+    for i, spec in enumerate(sp.Spec.all()):
+        data[i]['pen'] = spec.light_penetration
+        data[i]['monod'] = spec.light_monod
+        data[i]['mass'] = an.mass.get_by_spec(spec)['mean']
+        data[i]['perimeter'] = an.perimeter.get_by_spec(spec)['mean']
+        data[i]['hsa'] = an.horizontal_surface_area.get_by_spec(spec)['mean']
+        data[i]['convex_density'] = an.convex_density.get_by_spec(spec)['mean']
+        data[i]['cell_height'] = an.mean_cell_height.get_by_spec(spec)['mean']
+        print i, 
 
+    np.save('dump/pen_vs_light_monod')
 
-def get_light_indep_baseline(n=10, fields=['mass', 'convex_density', 'perimeter', 'horizontal_surface_area']):
-    util.set_h5(util.results_h5_path('pen_vs_light_monod_base'))
+if command == 'baseline':
+    def get_light_indep_baseline(n=10, fields=['mass', 'convex_density', 'perimeter', 'horizontal_surface_area']):
+        util.set_h5(util.results_h5_path('pen_vs_light_monod_base'))
 
-    results = []
-    for _ in range(n):
-        spec = sp.Spec(stop_on_mass=4000, stop_on_time=50000, stop_on_no_growth=1000,
-                       boundary_layer=5, light_penetration=0, tension_power=1, 
-                       distance_power=2, media_ratio=1, media_monod=0.25)
-        spec.save()
-        res = result.from_model(runner.run(spec))
-        res.save()
-        results.append(res)
+        results = []
+        for _ in range(n):
+            spec = sp.Spec(stop_on_mass=4000, stop_on_time=50000, stop_on_no_growth=1000,
+                           boundary_layer=5, light_penetration=0, tension_power=1, 
+                           distance_power=2, media_ratio=1, media_monod=0.25)
+            spec.save()
+            res = result.from_model(runner.run(spec))
+            res.save()
+            results.append(res)
 
-    means = {}
-    stds = {}
-    for field in fields:
-        func = getattr(an, field).func
-        values = np.array([func(res) for res in results])
-        means[field] = values.mean()
-        stds[field] = values.std()
+        means = {}
+        stds = {}
+        for field in fields:
+            func = getattr(an, field).func
+            values = np.array([func(res) for res in results])
+            means[field] = values.mean()
+            stds[field] = values.std()
 
-    return means, std
+        return means, std
 
-if False:
     means = get_light_indep_baseline()
     with open('plots/pen_vs_light_monod-means.cache', 'wb') as f:
         pickle.dump(means, f)
 
-with open('plots/pen_vs_light_monod-means.cache', 'rb') as f:
-    baseline_means = pickle.load(f)
+if command == 'plot':
+    data = np.load('dump/pen_vs_light_monod_base')
 
-print baseline_means
+    with open('plots/pen_vs_light_monod-means.cache', 'rb') as f:
+        baseline_means = pickle.load(f)
 
-query = 'light_penetration > 2' # '(light_penetration >= 0.1) & (light_penetration <= 16)'
+    print baseline_means
 
-# MASS
-if True:
-    # with plot_to_file('pen_vs_light_monod/mass'):
-    #     an.mass.phase_diagram_2d('light_penetration', 'light_monod',
-    #                              num_cells=(75, 50), spec_query=query)
-    # with plot_to_file('pen_vs_light_monod/mean_cell_height'):
-    #     an.mean_cell_height.phase_diagram_2d('light_penetration', 'light_monod',
-    #                              num_cells=(75, 50), spec_query=query)
+    query = 'light_penetration > 2' # '(light_penetration >= 0.1) & (light_penetration <= 16)'
+    
+    def plot_phase(field, num_cells=(64, 30)):
+        xs = data['pen']
+        ys = data['monod']
+        values = data[field]
+
+        xMin, xMax = xs.min(), xs.max()
+        yMin, yMax = ys.min(), ys.max()
+        
+        assert xMin != xMax
+        assert yMin != yMax
+
+        try:
+            num_x, num_y = num_cells
+        except TypeError:
+            num_x, num_y = num_cells, num_cells
+        
+        grid = np.mgrid[xMin:xMax:num_x*1j, 
+                        yMin:yMax:num_y*1j]
+        interp = interpolate.griddata(np.hstack((xs, ys)), 
+                                      values, 
+                                      np.vstack((grid[0].flat, grid[1].flat)).T, 
+                                      'linear')
+        valueGrid = np.reshape(interp, grid[0].shape)
+        
+        plt.pcolormesh(grid[0], grid[1], valueGrid, **plot_args)
+        plt.xlim(xMin, xMax)
+        plt.ylim(yMin, yMax)
+        plt.colorbar()
+
     with plot_to_file('pen_vs_light_monod/perimeter'):
-        an.perimeter.phase_diagram_2d('light_penetration', 'light_monod',
-                                 num_cells=(30, 30), spec_query=query)
-    with plot_to_file('pen_vs_light_monod/hsa'):
-        an.horizontal_surface_area.phase_diagram_2d('light_penetration', 'light_monod',
-                                 num_cells=(30, 30), spec_query=query)
+        plot_phase('perimeter')
 
 
