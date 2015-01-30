04e04767c7e33107cf527a96450cb2fae2725fe3
Ubuntu <chase.kernan@gmail.com>
2015-01-30 01:11:42 +0000

    Update search

--- biofilm/search/sample_v2.py
+++ biofilm/search/sample_v2.py
@@ -3,7 +3,9 @@
 from biofilm.model import spec, runner
 import numpy as np
 import sys
-import csv
+from boto.dynamodb2.table import Table
+from boto.dynamodb2 import connect_to_region
+import uuid
 
 param_ranges = {
     'stop_on_mass': (0, 5000),
@@ -23,10 +25,7 @@ param_ranges = {
     'height': 40,
 }
 
-
-num_to_run = int(sys.argv[1])
-results_path = sys.argv[2]
-
+table = Table('biofilm-parameter-space', connect_to_region('us-west-2'))
 
 def sample_spec():
     params = {}
@@ -46,27 +45,21 @@ def sample_spec():
 
     return spec.Spec(**params)
 
-with open(results_path, 'w') as f:
-    spec_fields = ['stop_on_mass', 'boundary_layer', 'light_penetration', 'distance_power',
-                   'tension_power', 'distance_power', 'initial_cell_spacing', 'media_ratio',
-                   'media_monod', 'light_monod', 'division_rate']
-    result_fields = ['biomass', 'coverage', 'time']
+spec_fields = ['stop_on_mass', 'boundary_layer', 'light_penetration', 'distance_power',
+	   'tension_power', 'distance_power', 'initial_cell_spacing', 'media_ratio',
+	   'media_monod', 'light_monod', 'division_rate']
+result_fields = ['biomass', 'coverage', 'time']
 
-    writer = csv.DictWriter(f, fieldnames=spec_fields+result_fields)
-    writer.writeheader()
-
-    for i in range(num_to_run):
-        print i,
-
-        s = sample_spec()
-        result = runner.run(s)
-        
-        row = {}
-        for field in spec_fields: row[field] = s[field]
-        row['biomass'] = result.mass/float(spec['width'])
-        row['coverage'] = result.cells.sum(axis=1)/float(spec['width'])
-        row['time'] = result.time
-        writer.writerow(row)
+for i in range(1000000):
+    s = sample_spec()
+    result = runner.run(s)
+    
+    row = { 'id': uuid.uuid4().hex }
+    for field in spec_fields: row[field] = str(getattr(s, field))
+    row['biomass'] = str(result.mass/float(s.width))
+    row['coverage'] = str(result.cells.sum(axis=1)/float(s.width))
+    row['time'] = str(result.time)
+    print table.put_item(data=row), i
 
 
 
